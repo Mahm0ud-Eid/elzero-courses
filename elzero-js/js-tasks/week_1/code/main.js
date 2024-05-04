c81a843e943979886b0e339602a999c99a58e09d
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2024-05-04 11:02:59 +0300

    done reviewing week_1 tasks

---
+++ elzero-js/js-tasks/week_1/code/main.js
@@ -0,0 +1,17 @@
+document.write("<h1>Hello <span>page</span></h1>");
+// console.log("Iam In Console");
+document.write("Iam In Page ");
+
+document.write("<h2>Elzero</h2>");
+document.querySelector("h2").style.color = "blue";
+document.querySelector("h2").style.fontSize = "80px";
+document.querySelector("h2").style.fontWeight = "bold";
+document.querySelector("h2").style.fontFamily = "arial";
+document.querySelector("h2").style.textAlign = "center";
+
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+/* console.log("Iam In Console"); 
+document.write("Iam In Page"); 
+ */
