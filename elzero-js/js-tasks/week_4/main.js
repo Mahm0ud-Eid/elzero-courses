719ee9d97692c055e8822b2e1beeaf5680a117a9
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2024-05-09 10:03:33 +0300

    done reviewing week_5

--- elzero-js/js-tasks/week_4/main.js
+++ elzero-js/js-tasks/week_4/main.js
@@ -25,41 +25,6 @@ console.log(Number.MAX_SAFE_INTEGER); // here you're interaction with values are
 console.log(Number.MAX_VALUE); // Maximum number value in JS that you cannot access beyond
 */
 
-/* //Lesson 2: Number Methods
-console.log((100).toString()); // converts the number to string]
-console.log((100).toString()); // same here
-console.log((100.1).toString()); // same same here :)
-console.log((100.555).toFixed(2)); // the number with two digits after the 'dot'
-console.log(parseInt("100")); // the opposite of all the above. It converts the string to integer
-console.log(+"100 osama"); // NaN
-console.log(Number("100 osama")); // NaN
-console.log(parseInt("100 osama")); // 100 as number
-console.log(parseInt("osama 100 ")); // 100 as number
-console.log(parseFloat(" 100.98 ")); // same as 'parseInt' but returns float
-console.log(Number.isInteger("100")); // false
-console.log(Number.isInteger(100.55)); // false
-console.log(Number.isInteger(100)); // true
-
-console.log(Number.isNaN("osama")); // false: it deals with NaN as value that has specific condition to be true. yes "osama" is not a number but it's not NaN. it's actually a string
-console.log(Number.isNaN("osama" / 20)); // true: Now this is NaN!. It's not string (not after dividing it by 20) it's like a number datatype but it's not a number. so, NaN! :)
- */
-
-/* //Lesson 3: Math Object
-console.log(Math.round(99.2)); //99
-console.log(Math.round(99.5)); //100
-console.log(Math.ceil(99.2)); //100
-console.log(Math.floor(99.8)); //99
-
-console.log(Math.min(10, 20, 100, -100, 90)); //-100
-console.log(Math.max(10, 20, 100, -100, 90)); //100
-
-console.log(Math.pow(2, 4)); //16
-
-console.log(Math.random()); //random numbers after '.'
-
-console.log(Math.trunc(99.99)); // returns only the integer value (the numbers before '.')
- */
-
 /* //Challenge 1
 let a = 100;
 let b = 2_00.5;
@@ -78,48 +43,6 @@ console.log(parseInt(d)); // 2
 // Use Variables b + d To Get This Valus
 console.log((parseInt(b) / Math.ceil(d)).toFixed(2)); // 66.67 => String
 console.log(Math.round(parseInt(b) / Math.ceil(d))); // 67 => Number
- */
-
-/* // Lesson 4: String Methods
-let theName = " Ahmed ";
-let theList = [1, 2, 3, 4, 5];
-
-console.log(theName);
-console.log(theName.charAt(5)); // if the number is outside the string's indexes, then it will not out anything
-console.log(theName[5]); // if the number is outside the string's indexes, then it will out 'undefined'
-console.log(theName.length);
-console.log(theName.trim()); // removes white spaces from the beginning and the end.
-console.log(theName.trim().charAt(2).toUpperCase()); // retrieving the letter 'M' in uppercase
- */
-
-/* // lesson 5: String Methods II
-let a = "Elzero Web School";
-
-console.log(a.indexOf("Web")); // the index of the first accurance of the value specified
-console.log(a.lastIndexOf("o")); //15 --> the last accurance of the value
-console.log(a.slice(0, 6));
-console.log(a.slice(-6)); // will start counting from the end till it approaches the index, then slice to the end (in this case)
-console.log(a.repeat(2)); // repeat the logging 'a' 2 times
-console.log(a.split()); // split it to an array, if the separator is defined it will separate every part of it based on the separator. (when specifing the separator, every accurance of the separator will cut the content before)
-console.log(a.split(" ")); // split it to an array, if the separator is defined it will separate every part of it based on the separato. (when specifing the separator, every accurance of the separator will cut the content before)
-console.log(a.split(" ", 2)); // limits the number of cutting 'spliting' to just 2 times, and everycontent after it will be ignored (dividing the string into an array with 2 elements)
- */
-
-/* // lesson 6: String Methods III
-let a = "Elzero Web School";
-
-console.log(a.length);
-console.log(a.substring(2, 6)); // the start index is must
-console.log(a.substring(6, 2)); // if the start index is higher than the end index, it'll swap them. in this case: same output.
-console.log(a.substring(-10, 6)); // if the start was negative, it will force it to start from 0.
-console.log(a.substring(a.length - 1)); // just output the last index
-console.log(a.substring(a.length - 5, a.length - 3)); // doing the same as 'slice'
-console.log(a.substr(0, 2)); // the start is must, and then counting (NOT specifing the end) the indexes to stop
-console.log(a.substr(18, 2)); // if the start was a higher value than the length, it will out an empty string
-console.log(a.includes("Web")); // checks ..., then true or false, the second parameter is optional, which is the position I start search from
-console.log(a.startsWith("zero", 2)); // if the second parameter specified, it will check if your string 'at that position' starts with the value specified or not, it can be an string 'not just a character'
-console.log(a.endsWith("ro", 6)); // the same but the second parameter is length not a position (start counting from the start till reaches the number specified, at this point, it ends with the value specified or not?)
-console.log(a.endsWith("k")); // if the second parameter is not specified, it will check if the whole string ends with the value specified or not.
  */
 
 /* // Challenge 2:
@@ -191,6 +114,16 @@ console.log(Math.round((Math.random()*4)));
  */
 
 /* //part 2 Assignment 1
+let userName = "Elzero";
+console.log(userName.substring(0, 1).toLowerCase());
+console.log(userName.substr(0, 1).toLowerCase()); // e
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.slice(0, 1).toLowerCase()); // e
+console.log(userName.trim().charAt(0).toLowerCase().repeat(3)); // eee
+ */
+
+/* //part 2 Assignment 2
 let word = "Elzero";
 let letterZ = "z";
 let letterE = "e";
@@ -199,14 +132,4 @@ let letterO = "O";
 console.log(word.includes(letterZ)); // True
 console.log(word.startsWith(letterE.toUpperCase())); // True
 console.log(word.endsWith(letterO.toLowerCase())); // True
- */
-
-/* //part 2 Assignment 2
-let userName = "Elzero";
-console.log(userName.substring(0, 1).toLowerCase());
-console.log(userName.substr(0, 1).toLowerCase()); // e
-console.log(userName.charAt(0).toLowerCase()); // e
-console.log(userName[0].toLowerCase()); // e
-console.log(userName.slice(0, 1).toLowerCase()); // e
-console.log(userName.trim().charAt(0).toLowerCase().repeat(3)); // eee
  */
--- elzero-js/js-tasks/week_5/main.js
+++ elzero-js/js-tasks/week_5/main.js
@@ -1,193 +1,3 @@
-/* let a = 10;
-
-a < 10 ? 
-    console.log(10) 
-: a >= 10 && a <= 40 ? 
-    console.log("10 to 40") 
-: a > 40 ? 
-    console.log("> 40") 
-: console.log("Unknown") challenge 1 */
-
-/* let st = "Elzero Web School";
-
-if ((st.length*2).toString() === "34") {
-    console.log("Good")
-}
-
-if ((st.charAt(st.indexOf("W"))).toLowerCase() || (st.charAt(st.indexOf("W"))) === "W") {
-    console.log("Good")
-}
-//
-if (st !== "string") {
-    console.log("Good")
-}
-
-if ((typeof((!((typeof st)).toString())+1)).toString() === "number") {
-    console.log("Good")
-}
-
-if (st.slice(0, 6).repeat(2) === "ElzeroElzero") {
-    console.log("Good")
-} challenge 2 */
-
-/* let job = "jdk";
-let salary = 0;
-
-switch (job) {
-    case "Manager":
-        salary = 8000;
-        console.log(salary); break;
-    case "IT":
-    case "Support":
-        salary = 6000;
-        console.log(salary); break;
-    case "Developer":
-    case "Designer":
-        salary = 7000;
-        console.log(salary); break;
-    default:
-        salary = 6000;
-        console.log(salary); break;
-
-}
-
- challenge 3 */
-
-/* let holidays = 0;
-let money = 0;
-
-if (holidays === 0) {
-    money = 5000;
-    console.log(`My Money is ${money}`);
-} 
-else if (holidays === 1 || holidays === 2) {
-    money = 3000;
-    console.log(`My Money is ${money}`);
-} 
-else if (holidays === 3) {
-    money = 2000;
-    console.log(`My Money is ${money}`);
-}
-else if (holidays === 4) {
-    money = 1000;
-    console.log(`My Money is ${money}`);
-}
-else if (holidays === 5) {
-    money = 0;
-    console.log(`My Money is ${money}`);
-}
-else {
-    money = 0;
-    console.log(`My Money is ${money}`);
-}  challenge 4 */
-
-/* ------------------------------------------------------------------------- */
-/* // Challenge 1
-  // - 1
-  let a = 10;
-  a < 10
-    ? console.log(10)
-    : a >= 10 && a <= 40 
-    ? console.log("10 To 40")
-    : a > 40
-    ? console.log("> 40")
-    : console.log("Unknown");
-
-    
-let st = "Elzero Web School";
-// - 2
-// 1
-if (st.charAt(st.indexOf('W')).toLowerCase() === "w") {
-  console.log("Good");
-}
-
-// 2
-if (typeof(st.length) !== "string") {
-  console.log("Good");
-}
-
-//3
-if (typeof(st.length) === "number") {
-  console.log("Good");
-}
-
-// 4
-if (st.slice(0, 6).repeat(2)=== "ElzeroElzero") {
-  console.log("Good");
-} 
-*/
-
-/* ------------------------------------------------------------------------- */
-// Switch Statement Challenge
-// 1st
-
-/* let job = "Manager";
-let salary = 0;
-
-if (job === "Manager") {
-  salary = 8000;
-} else if (job === "IT" || job === "Support") {
-  salary = 6000;
-} else if (job === "Developer" || job === "Designer") {
-  salary = 7000;
-} else {
-  salary = 4000;
-}
-
-switch (job) {
-  case "Manager": salary = 8000; 
-    console.log(`salary is ${salary}`); break;
-  case "IT":
-  case "Support": salary = 6000; 
-    console.log(`salary is ${salary}`); break;
-  case "Developer":
-  case "Designer": salary = 7000; 
-      console.log(`salary is ${salary}`); break;
-  default: salary = 4000;
-    console.log(`salary is ${salary}`); break;
-}
- */
-
-// 2nd
-/*     let holidays = 4;
-    let money = 0;
-    
-    switch (holidays) {
-      case 0: money = 5000; console.log(`My Money is ${money}`); break;
-      case 1:
-      case 2: money = 3000; console.log(`My Money is ${money}`); break;
-      case 3: money = 2000; console.log(`My Money is ${money}`); break;
-      case 4: money = 1000; console.log(`My Money is ${money}`); break;
-      case 5: money = 0; console.log(`My Money is ${money}`); break;
-      default: money = 0; console.log(`My Money is ${money}`);
-    }
-
-    if (holidays === 0) {
-      money = 5000;
-      console.log(`My Money is ${money}`);
-    }
-    else if (holidays === 1 || holidays === 2) {
-      money = 3000;
-      console.log(`My Money is ${money}`);
-    }
-    else if (holidays === 3) {
-      money = 2000;
-      console.log(`My Money is ${money}`);
-    }
-    else if (holidays === 4) {
-      money = 1000;
-      console.log(`My Money is ${money}`);
-    }
-    else if (holidays === 5) {
-      money = 0;
-      console.log(`My Money is ${money}`);
-    }
-    else {
-      money = 0;
-      console.log(`My Money is ${money}`);
-    }
- */
-
 /* // Assignment 1-1
 console.log(100 == "100"); // true
 console.log(100 != 1000); // true
@@ -357,3 +167,192 @@ switch (day) {
     break;
 }
  */
+
+/* ------------------------------------------------------------------------- */
+/* // Challenge 1
+  // - 1
+  let a = 10;
+  a < 10
+    ? console.log(10)
+    : a >= 10 && a <= 40 
+    ? console.log("10 To 40")
+    : a > 40
+    ? console.log("> 40")
+    : console.log("Unknown");
+
+    
+let st = "Elzero Web School";
+// - 2
+// 1
+if (st.charAt(st.indexOf('W')).toLowerCase() === "w") {
+  console.log("Good");
+}
+
+// 2
+if (typeof(st.length) !== "string") {
+  console.log("Good");
+}
+
+//3
+if (typeof(st.length) === "number") {
+  console.log("Good");
+}
+
+// 4
+if (st.slice(0, 6).repeat(2)=== "ElzeroElzero") {
+  console.log("Good");
+} 
+*/
+
+/* ------------------------------------------------------------------------- */
+// Switch Statement Challenge
+// 1st
+
+/* let job = "Manager";
+let salary = 0;
+
+if (job === "Manager") {
+  salary = 8000;
+} else if (job === "IT" || job === "Support") {
+  salary = 6000;
+} else if (job === "Developer" || job === "Designer") {
+  salary = 7000;
+} else {
+  salary = 4000;
+}
+
+switch (job) {
+  case "Manager": salary = 8000; 
+    console.log(`salary is ${salary}`); break;
+  case "IT":
+  case "Support": salary = 6000; 
+    console.log(`salary is ${salary}`); break;
+  case "Developer":
+  case "Designer": salary = 7000; 
+      console.log(`salary is ${salary}`); break;
+  default: salary = 4000;
+    console.log(`salary is ${salary}`); break;
+}
+ */
+
+// 2nd
+/*     let holidays = 4;
+    let money = 0;
+    
+    switch (holidays) {
+      case 0: money = 5000; console.log(`My Money is ${money}`); break;
+      case 1:
+      case 2: money = 3000; console.log(`My Money is ${money}`); break;
+      case 3: money = 2000; console.log(`My Money is ${money}`); break;
+      case 4: money = 1000; console.log(`My Money is ${money}`); break;
+      case 5: money = 0; console.log(`My Money is ${money}`); break;
+      default: money = 0; console.log(`My Money is ${money}`);
+    }
+
+    if (holidays === 0) {
+      money = 5000;
+      console.log(`My Money is ${money}`);
+    }
+    else if (holidays === 1 || holidays === 2) {
+      money = 3000;
+      console.log(`My Money is ${money}`);
+    }
+    else if (holidays === 3) {
+      money = 2000;
+      console.log(`My Money is ${money}`);
+    }
+    else if (holidays === 4) {
+      money = 1000;
+      console.log(`My Money is ${money}`);
+    }
+    else if (holidays === 5) {
+      money = 0;
+      console.log(`My Money is ${money}`);
+    }
+    else {
+      money = 0;
+      console.log(`My Money is ${money}`);
+    }
+ */
+
+/* let a = 10;
+
+a < 10 ? 
+    console.log(10) 
+: a >= 10 && a <= 40 ? 
+    console.log("10 to 40") 
+: a > 40 ? 
+    console.log("> 40") 
+: console.log("Unknown") challenge 1 */
+
+/* let st = "Elzero Web School";
+
+if ((st.length*2).toString() === "34") {
+    console.log("Good")
+}
+
+if ((st.charAt(st.indexOf("W"))).toLowerCase() || (st.charAt(st.indexOf("W"))) === "W") {
+    console.log("Good")
+}
+//
+if (st !== "string") {
+    console.log("Good")
+}
+
+if ((typeof((!((typeof st)).toString())+1)).toString() === "number") {
+    console.log("Good")
+}
+
+if (st.slice(0, 6).repeat(2) === "ElzeroElzero") {
+    console.log("Good")
+} challenge 2 */
+
+/* let job = "jdk";
+let salary = 0;
+
+switch (job) {
+    case "Manager":
+        salary = 8000;
+        console.log(salary); break;
+    case "IT":
+    case "Support":
+        salary = 6000;
+        console.log(salary); break;
+    case "Developer":
+    case "Designer":
+        salary = 7000;
+        console.log(salary); break;
+    default:
+        salary = 6000;
+        console.log(salary); break;
+
+}
+ challenge 3 */
+
+/* let holidays = 0;
+let money = 0;
+
+if (holidays === 0) {
+    money = 5000;
+    console.log(`My Money is ${money}`);
+} 
+else if (holidays === 1 || holidays === 2) {
+    money = 3000;
+    console.log(`My Money is ${money}`);
+} 
+else if (holidays === 3) {
+    money = 2000;
+    console.log(`My Money is ${money}`);
+}
+else if (holidays === 4) {
+    money = 1000;
+    console.log(`My Money is ${money}`);
+}
+else if (holidays === 5) {
+    money = 0;
+    console.log(`My Money is ${money}`);
+}
+else {
+    money = 0;
+    console.log(`My Money is ${money}`);
+}  challenge 4 */
