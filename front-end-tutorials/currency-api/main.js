e72ccd408fa78aa2e3f5c78704dfa1ff35c3c62e
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2024-07-15 19:54:35 +0300

    some modifications on tutorial: How To Use Currency API

--- front-end-tutorials/currency-api/main.js
+++ front-end-tutorials/currency-api/main.js
@@ -8,14 +8,30 @@ fetch(
     return myData;
   })
   .then((currency) => {
+    // egpPrice.innerHTML = Math.round(amount.innerHTML * currency.rates["EGP"]);
+    // sarPrice.innerHTML = Math.round(amount.innerHTML * currency.rates["SAR"]);
+    // // console.log(currency.rates);
+    // console.log(currency.rates["EGP"]);
+    // console.log(currency.rates["SAR"]);
+  });
+
+async function fetchData() {
+  try {
     let amount = document.querySelector(".amount");
     let egpPrice = document.querySelector(".egp span");
     let sarPrice = document.querySelector(".sar span");
 
+    let myData = await fetch(
+      "https://api.currencyfreaks.com/latest?apikey=f030cd26ef124bcfad3c05931f597426"
+    );
+
+    let currency = await myData.json();
     egpPrice.innerHTML = Math.round(amount.innerHTML * currency.rates["EGP"]);
     sarPrice.innerHTML = Math.round(amount.innerHTML * currency.rates["SAR"]);
+  } catch (reason) {
+    console.log(`Reason: ${reason}`);
+  } finally {
+  }
+}
 
-    // console.log(currency.rates);
-    console.log(currency.rates["EGP"]);
-    console.log(currency.rates["SAR"]);
-  });
+fetchData();
